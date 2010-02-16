d6ef6466e18157279d5c6b41d2001a2664987383
marinaSantini <marinaromestockholm@gmail.com>
2010-02-16 16:44:56 +0100

    sql for storing applicant, unfinished

---
+++ store_applicant.sql
@@ -0,0 +1,24 @@
+INSERT INTO applicants (
+						personal_code, 
+						surname, 
+						name, 
+						c/o_address, 
+						street_address, 
+						postal_code, 
+						postal_area 
+						e_mail, 
+						phone_number, 
+						mobile_phone_number
+VALUES (
+						null,
+						null,
+						null,
+						null,
+						null,
+						null,
+						null,
+						null,
+						null,
+						null
+	
+		)
