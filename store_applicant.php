ef46c9889b08a90fba48187ad2b4aa4e5c1d4979
Lavinia <forgetmenotox@notingham.se>
2010-02-17 15:01:47 +0100

    lite stavfel

--- store_applicant.php
+++ store_applicant.php
@@ -3,16 +3,16 @@
 function store_applicant($input) {
 	
 	$query = "INSERT INTO applicant
-			 	`personal_number` = '{$input["personal_number"]}',
-				`surname` = '{$input["surname"]}', 
-				`firstname` = '{$input["firstname"]}', 
-				`co_address` = '{$input["co_address"]}', 
-				`address` = '{$input["address"]}', 
-				`postal_code` = '{$input["postal_code"]}',
-				`postal_area` = '{$input["postal_area"]}', 
-				`telephone` =  '{$input["telephone"]}', 
-				`mobile` = '{$input["mobile"]}', 
-				`e_mail` = '{$input["e_mail"]}';
+			 		`personal_number` = '{$input['personal_number']}',
+					`surname` = '{$input['surname']}', 
+					`firstname` = '{$input['firstname']}', 
+					`co_address` = '{$input['co_address']}', 
+					`address` = '{$input['address']}', 
+					`postal_code` = '{$input['postal_code']}',
+					`postal_area` = '{$input['postal_area']}', 
+					`telephone` =  '{$input['telephone']}', 
+					`mobile` = '{$input['mobile']}', 
+					`e_mail` = '{$input['e_mail']}'";
     
 	$answer = mysql_query($query);
     return $answer;
