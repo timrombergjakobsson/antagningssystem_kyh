c4ac94e454b63f85571a055fd192b52788c1a073
Lavinia <forgetmenotox@notingham.se>
2010-02-18 16:55:40 +0100

    app.info...

---
+++ application_info.php
@@ -0,0 +1,34 @@
+<?php
+    
+    function chosen_education_exp () {
+    
+        $chosen_educations = array ( 'education_1' => array (
+	 														'priority' => 1, 'education' => $_POST['education_1']));
+        
+        if ($_POST['education_2'] != "Ingen") {
+    
+            $chosen_educations['education_2'] = array ('priority' => 2, 'education' => $_POST['education_2']);
+        
+        }
+        
+        if ($_POST['education_3'] != "Ingen") {
+        
+            $chosen_educations['education_3'] = array ('priority' => 3, 'education' => $_POST['education_3']);
+            
+        }
+
+		$exploded_educations;
+		$i = 0;
+		foreach($chosen_educations as $looped_education) {
+			list($name, $city) = explode("," , $looped_education['education']);
+			$exploded_educations[$i]['education_name'] = $name;
+			$exploded_educations[$i]['education_city'] = $city;
+			$exploded_educations[$i]['priority'] = $looped_education['priority'];
+	
+			$i++;
+		}
+
+	
+	}
+
+?>
