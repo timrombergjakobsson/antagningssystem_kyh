8718c341b9f05bddbc91fafe2d812f49a9fc2ff5
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-02-19 16:23:49 +0100

    function that handles the mysql_query errors

---
+++ handle_mysql_query.php
@@ -0,0 +1,24 @@
+<?php
+	
+	/* funktion som hanterar mysql_query errors och returnerar en resultset på dessa*/
+	
+	function handle_myquery ($query) {
+	        $result = mysql_query($query);
+	        if (mysql_errno()) {
+		
+	            echo "MySQL error ".mysql_errno().": ".mysql_error().
+					 "\n<br>When executing:<br>\n$query\n<br>"; 
+			}
+	        
+				return $result;
+	  }
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
