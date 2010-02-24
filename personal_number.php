672beffef4eac1d9f061c076be501efa2684e092
marinaSantini <marinaromestockholm@gmail.com>
2010-02-24 11:41:01 +0100

    form for checking personal_number

---
+++ personal_number.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+	<<??>?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+		"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<title>Person nummer</title>
+		<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
+
+	</head>
+
+	<body>
+
+		<h1></h1>
+		<div id="person_nummer">
+
+
+
+
+		<form action="" method="post">
+
+			<fieldset>
+			<legend>Skriva person nummer</legend>
+
+				<label for="personal_number">Person nummer</label>
+				<input type ="text" id="personal_number" name="personal_number" />
+
+		</fieldset>	
+		</form>
+		</div>
+	</body>
+	</html>
