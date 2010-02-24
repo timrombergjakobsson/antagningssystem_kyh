59363df2df486d72d768db83aab3a9780f760fbb
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-02-24 11:31:19 +0100

    new db_connect_function

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,20 @@
-<?php>
+<?php
 
-	function db_connect(){
+function db_connect(){
 	
-	$dbhost = 'reldb4.ad09.agiledev.se';
-	$dbuser = 'reldb4';
-	$dbpass = 'dbPass04';
-	
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-	if (!$conn) {
-	    die('Could not connect: ' . mysql_error());
-	}else{
-	echo 'Connected successfully';
-	}
-	mysql_close($conn);
-	
-	}
+			$dbhost = "localhost";
+			$dbuser = "reldb4";
+			$dbpass = "dbPass04";
+			$dbname = "ad09_reldb4";
+
+			$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+			$db_selected = mysql_select_db ($dbname, $conn);
+			if (!$conn || !$db_selected) {
+	    		die('Could not connect: ' . mysql_error());
+				
+			}else{
+				echo 'Connected successfully';
+				
+			}
+			return $conn;
+		}
