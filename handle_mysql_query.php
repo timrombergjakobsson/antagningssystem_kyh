d26d8ca8369515688f4aa13b2ea58afe3c2ed4d5
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-02-25 14:51:30 +0100

    changes to handle_mysql_query.php

--- handle_mysql_query.php
+++ handle_mysql_query.php
@@ -2,15 +2,17 @@
 	
 	/* funktion som hanterar mysql_query errors och returnerar en resultset på dessa*/
 	
-	function handle_mysql_query ($query) {
-	        $result = mysql_query($query);
+	function handle_mysql_query ($query, $conn) {
+	        $result = mysql_query($query, $conn);
 	        if (mysql_errno()) {
 		
 	            echo "MySQL error ".mysql_errno().": ".mysql_error().
 					 "\n<br>When executing:<br>\n$query\n<br>"; 
+			} else {
+				return $result;
 			}
 	        
-				return $result;
+				
 	  }
 	
 		
