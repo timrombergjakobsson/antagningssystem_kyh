7d287eb7fded5d20b3df382e26b6823ef7e1eee2
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-02-25 15:23:07 +0100

    updated application_info_form.php

--- application_info_form.php
+++ application_info_form.php
@@ -1,91 +1,76 @@
 <<??>?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+		"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-	<title>Ans&ouml;kan</title>
-	<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
-	
-</head>
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<title>Ans&ouml;kan</title>
+		<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
 
-<body>
-	
-	<h1></h1>
-	<div id="Ans&ouml;kaninformation">
-	<?php
+	</head>
 
-		function db_connect(){
-	
-			$dbhost = "localhost";
-			$dbuser = "reldb4";
-			$dbpass = "dbPass04";
-			$dbname = "ad09_reldb4";
+	<body>
 
-			$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-			$db_selected = mysql_select_db ($dbname, $conn);
-			if (!$conn || !$db_selected) {
-	    		die('Could not connect: ' . mysql_error());
+		<h1></h1>
+		<div id="Ans&ouml;kaninformation">
+
+
+
+
+			<form action="" method="post">
+
+				<fieldset>
+					<legend>Ans&ouml;kan</legend>
+
+					<label for="arrival_date">Ans&ouml;kan ankom</label>
+					<input type ="text" id="arrival_date" name="arrival_date" />
+
+					<input type="hidden" id="admission_id" name="admission_id" value="2"/>
 				
-			}else{
-				echo 'Connected successfully';
-				
-			}
-			return $conn;
-		}
-	?>
-	<form action="" method="post">
-		
-		<fieldset>
-		<legend>Ansökan</legend>
+					<label for="education_1">Utbildning 1:a prio</label>
+					<select name="education_1" id="education_1">
+						<option>Agile Developer, Stockholm</option>
+						<option>Agile Developer, G&ouml;teborg</option>
+						<option>Energy Consultant, Stockholm</option>
+						<option>Energy Consultant, Malm&ouml;</option>
+						<option>IT Management, Stockholm</option>
 
-			<label for="arrival_date">Ansökan ankom</label>
-			<input type ="text" id="arrival_date" name="arrival_dates" />
-			
-			<label for="education_1">Utbildning 1:a prio</label>
-			<select name="education_1" id="education_1">
-				<option>Agile Developer, Stockholm</option>
-				<option>Agile Developer, Göteborg</option>
-				<option>Energy Consultant, Stockholm</option>
-				<option>Energy Consultant, Malmö</option>
-				<option>IT Management, Stockholm</option>
-				
-			</select>
-			
-			<label for="education_2">Utbildning 2:a prio</label>
-			<select name="education_2" id="education_2">
-				<option>Ingen</option>
-				<option>Agile Developer, Stockholm</option>
-				<option>Agile Developer, Göteborg</option>
-				<option>Energy Consultant, Stockholm</option>
-				<option>Energy Consultant, Malmö</option>
-				<option>IT Management, Stockholm</option>
+					</select>
 
-			</select>
-			
-			<label for="education_3">Utbildning 3:e prio</label>
-			<select name="education_3" id="education_3">
-				<option>Ingen</option>
-				<option>Agile Developer, Stockholm</option>
-				<option>Agile Developer, Göteborg</option>
-				<option>Energy Consultant, Stockholm</option>
-				<option>Energy Consultant, Malmö</option>
-				<option>IT Management, Stockholm</option>
+					<label for="education_2">Utbildning 2:a prio</label>
+					<select name="education_2" id="education_2">
+						<option>Ingen</option>
+						<option>Agile Developer, Stockholm</option>
+						<option>Agile Developer, G&ouml;teborg</option>
+						<option>Energy Consultant, Stockholm</option>
+						<option>Energy Consultant, Malm&ouml;</option>
+						<option>IT Management, Stockholm</option>
 
-			</select>
+					</select>
 
-			<label for="basic_eligibility">Grundläggande behörighet</label>
-			<select name="basic_eligibility" id="basic_eligibility">
-				<option>Ja</option>
-				<option>Nej</option>
-				<option>Dispens</option>
-			</select>
-			
-			<input type="submit" name="submit" value="spara" />
+					<label for="education_3">Utbildning 3:e prio</label>
+					<select name="education_3" id="education_3">
+						<option>Ingen</option>
+						<option>Agile Developer, Stockholm</option>
+						<option>Agile Developer, G&ouml;teborg</option>
+						<option>Energy Consultant, Stockholm</option>
+						<option>Energy Consultant, Malm&ouml;</option>
+						<option>IT Management, Stockholm</option>
 
-	
-	</fieldset>	
-	</form>
-	</div>
-</body>
-</html>
+					</select>
+
+					<label for="basic_eligibility">Grundl&auml;ggande beh&ouml;righet</label>
+					<select name="basic_eligibility" id="basic_eligibility">
+						<option>Ja</option>
+						<option>Nej</option>
+						<option>Dispens</option>
+					</select>
+
+					<input type="submit" name="submit" value="spara" />
+
+
+				</fieldset>	
+			</form>
+		</div>
+	</body>
+	</html>
