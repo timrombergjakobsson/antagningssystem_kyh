563bb7bc711b4e87ba44f1a17b99d4652940effd
Tim Jakobsson <tim.jakobsson@gmail.com>
2010-03-01 15:31:27 +0100

    adding index and store admission and store education_start functions

--- store_admission.php
+++ store_admission.php
@@ -1,23 +1,14 @@
 <?php
 
-	function store_admission ($input) {
-		
-		 $query = "INSERT INTO admission
-		`last_application_date` = '{$input["last_application_date"]}',
-		`last_completion_date` = '{$input["last_completion_date"]}',
-		`year` = '{$input["year"]}',
-		`semester` = '{$input["semester"]}'";
-
-		$answer = handle_mysql_query($query);
-		    return $answer;
-		}
-	}
-
-
+	function store_admission($input,$conn) {
 	
+	$query = "INSERT IGNORE INTO admission SET
+			 	`last_application_date` = '{$input["last_application_date"]}',
+				`last_completion_date` = '{$input["last_completion_date"]}', 
+				`year` = '{$input["year"]}', 
+				`semester` = '{$input["semester"]}'
+					"; 
 
-
-
-
-
-?>
+		$answer = handle_mysql_query($query,$conn);
+		return $answer;
+}
